a85750b13f045d11c802ccff02d908d19eedb656
Nick Robinson <grobins2@gmail.com>
2013-11-10 23:30:28 -0500

    updated docstring

--- flask_scrypt.py
+++ flask_scrypt.py
@@ -1,5 +1,4 @@
-"""
-Flask-Scrypt flask extension provides scrypt password hashing and random salt generation.
+""" Flask-Scrypt flask extension provides scrypt password hashing and random salt generation.
 Hashes and Salts are base64 encoded.
 """
 import sys
@@ -24,8 +23,8 @@ PYTHON3 = sys.version_info >= (3, 0)
 
 
 def enbase64(byte_str):
-    """
-    Encode bytes to base64
+    """ Encode bytes to base64
+
         Args:
             byte_str : bytes to encode
 
@@ -34,8 +33,8 @@ def enbase64(byte_str):
 
 
 def debase64(byte_str):
-    """
-    Decode base64 encoded bytes
+    """ Decode base64 encoded bytes
+
         Args:
             byte_str : bytes to decode
     """
@@ -48,17 +47,20 @@ def generate_password_hash(password, salt, N=1 << 14, r=8, p=1, buflen=64):
     """ Returns a base64 encoded password hash
 
         Args:
-            password (str): Password string
-            salt : Random base64 encoded string
-            N : the CPU cost, must be a power of 2 greater than 1, defaults to 1 << 14
-            r : the memory cost, defaults to 8
-            p : the parallelization parameter, defaults to 1
+            password (str): Password string.
+            salt : Random base64 encoded string.
+            N : the CPU cost, must be a power of 2 greater than 1, defaults to 1 << 14.
+            r : the memory cost, defaults to 8.
+            p : the parallelization parameter, defaults to 1.
 
             The parameters r, p, and buflen must satisfy r * p < 2^30 and
             buflen <= (2^32 - 1) * 32.
 
             The recommended parameters for interactive logins as of 2009 are N=16384,
             r=8, p=1. Remember to get a good random salt.
+
+        Returns:
+            base64 encoded scrypt hash.
     """
     password = password.encode('utf-8')
     scrypt_hash = scrypt.hash(password, salt, N, r, p, buflen)
@@ -70,16 +72,24 @@ def generate_random_salt(byte_size=64):
 
         Args:
             byte_size (int): The length of salt to return. default = 64
+
+        Returns:
+            base64 encoded random bytes
     """
     return enbase64(urandom(byte_size))
 
 
 def check_password_hash(password, password_hash, salt):
-    """ Given a password, hash, salt this function verifies the password
-        is equal to hash/salt.
+    """Given a password, hash, salt this function verifies the password is 
+    equal to hash/salt.
 
         Args:
             password (str): The password to check.
+
+        Returns:
+            boolean
+                True -- Correct password.
+                False -- Incorrect password.
     """
 
 
