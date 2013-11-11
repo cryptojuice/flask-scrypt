91baa0fc778e50cf447bf1f5e749d2dd9c279604
Nick Robinson <grobins2@gmail.com>
2013-11-11 00:03:50 -0500

    docstring update

--- flask_scrypt.py
+++ flask_scrypt.py
@@ -1,5 +1,4 @@
-""" Flask-Scrypt flask extension provides scrypt password hashing and random salt generation.
-Hashes and Salts are base64 encoded.
+"""Flask-Scrypt flask extension provides scrypt password hashing and random salt generation. Hashes and Salts are base64 encoded.
 """
 import sys
 import base64
@@ -25,8 +24,8 @@ PYTHON3 = sys.version_info >= (3, 0)
 def enbase64(byte_str):
     """Encode bytes to base64
 
-        Args:
-            byte_str : bytes to encode
+    Args:
+        byte_str : bytes to encode
 
     """
     return base64.b64encode(byte_str)
@@ -35,8 +34,8 @@ def enbase64(byte_str):
 def debase64(byte_str):
     """Decode base64 encoded bytes
 
-        Args:
-            byte_str : bytes to decode
+    Args:
+        byte_str : bytes to decode
     """
     if PYTHON3 and isinstance(byte_str, str):
         byte_str = bytes(byte_str, 'utf-8')
@@ -46,23 +45,23 @@ def debase64(byte_str):
 def generate_password_hash(password, salt, N=1 << 14, r=8, p=1, buflen=64):
     """Generate password hash givin the password string and salt.
 
-        Args:
-            password (str): Password string.
-            salt : Random base64 encoded string.
+    Args:
+        password (str): Password string.
+        salt : Random base64 encoded string.
 
-            Optional:
-                N : the CPU cost, must be a power of 2 greater than 1, defaults to 1 << 14.
-                r : the memory cost, defaults to 8.
-                p : the parallelization parameter, defaults to 1.
+        Optional:
+            N : the CPU cost, must be a power of 2 greater than 1, defaults to 1 << 14.
+            r : the memory cost, defaults to 8.
+            p : the parallelization parameter, defaults to 1.
 
-            The parameters r, p, and buflen must satisfy r * p < 2^30 and
-            buflen <= (2^32 - 1) * 32.
+    The parameters r, p, and buflen must satisfy r * p < 2^30 and
+    buflen <= (2^32 - 1) * 32.
 
-            The recommended parameters for interactive logins as of 2009 are N=16384,
-            r=8, p=1. Remember to get a good random salt.
+    The recommended parameters for interactive logins as of 2009 are N=16384,
+    r=8, p=1. Remember to get a good random salt.
 
-        Returns:
-            base64 encoded scrypt hash.
+    Returns:
+        base64 encoded scrypt hash.
     """
     password = password.encode('utf-8')
     scrypt_hash = scrypt.hash(password, salt, N, r, p, buflen)
@@ -70,27 +69,19 @@ def generate_password_hash(password, salt, N=1 << 14, r=8, p=1, buflen=64):
 
 
 def generate_random_salt(byte_size=64):
-    """Generate random salt
-    
-        Args:
-            byte_size (int): The length of salt to return. default = 64
-
-        Returns:
-            base64 encoded random bytes
-    """
-    return enbase64(urandom(byte_size))
+    salt = enbase64(urandom(byte_size))
+    return salt
 
 
 def check_password_hash(password, password_hash, salt):
     """Given a password, hash, salt this function verifies the password is equal to hash/salt.
+    Args:
+        password (str): The password to check.
 
-        Args:
-            password (str): The password to check.
-
-        Returns:
-            boolean
-                True -- Correct password.
-                False -- Incorrect password.
+    Returns:
+        boolean
+            True -- Correct password.
+            False -- Incorrect password.
     """
 
 
