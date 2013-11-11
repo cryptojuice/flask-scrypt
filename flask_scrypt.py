e3d6ad5d6f5b424bb88453a4667f141e14c82831
Nick Robinson <grobins2@gmail.com>
2013-11-11 01:40:03 -0500

    docstring reformatting

--- flask_scrypt.py
+++ flask_scrypt.py
@@ -22,20 +22,27 @@ PYTHON3 = sys.version_info >= (3, 0)
 
 
 def enbase64(byte_str):
-    """Encode bytes to base64
+    """
+    Encode bytes/strings to base64.
 
     Args:
-        byte_str : bytes to encode
+        - ``byte_str``:  The string or bytes to base64 encode.
 
+    Returns:
+        - byte_str encoded as base64.
     """
     return base64.b64encode(byte_str)
 
 
 def debase64(byte_str):
-    """Decode base64 encoded bytes
+    """
+    Decode base64 encoded bytes/strings.
 
     Args:
-        byte_str : bytes to decode
+        - ``byte_str``:  The string or bytes to base64 encode. 
+
+    Returns:
+        - decoded string as type str for python2 and type byte for python3.
     """
     if PYTHON3 and isinstance(byte_str, str):
         byte_str = bytes(byte_str, 'utf-8')
@@ -43,25 +50,26 @@ def debase64(byte_str):
 
 
 def generate_password_hash(password, salt, N=1 << 14, r=8, p=1, buflen=64):
-    """Generate password hash givin the password string and salt.
+    """
+    Generate password hash givin the password string and salt.
 
     Args:
-        password (str): Password string.
-        salt : Random base64 encoded string.
+        - ``password``: Password string.
+        - ``salt`` : Random base64 encoded string.
 
-        Optional:
-            N : the CPU cost, must be a power of 2 greater than 1, defaults to 1 << 14.
-            r : the memory cost, defaults to 8.
-            p : the parallelization parameter, defaults to 1.
+    Optional args:
+        - ``N`` : the CPU cost, must be a power of 2 greater than 1, defaults to 1 << 14.
+        - ``r`` : the memory cost, defaults to 8.
+        - ``p`` : the parallelization parameter, defaults to 1.
 
     The parameters r, p, and buflen must satisfy r * p < 2^30 and
     buflen <= (2^32 - 1) * 32.
 
     The recommended parameters for interactive logins as of 2009 are N=16384,
-    r=8, p=1. Remember to get a good random salt.
+    r=8, p=1. Remember to use a good random salt.
 
     Returns:
-        base64 encoded scrypt hash.
+        - base64 encoded scrypt hash.
     """
     password = password.encode('utf-8')
     scrypt_hash = scrypt.hash(password, salt, N, r, p, buflen)
@@ -69,27 +77,29 @@ def generate_password_hash(password, salt, N=1 << 14, r=8, p=1, buflen=64):
 
 
 def generate_random_salt(byte_size=64):
-    """Generate random salt to use with generate_password_hash().
+    """
+    Generate random salt to use with generate_password_hash().
 
-    Args:
-        byte_size (int): The length of salt to return. default = 64.
+    Optional Args:
+        - ``byte_size``: The length of salt to return. default = 64.
 
     Returns:
-        base64 encoded random bytes
+        - base64 encoded random bytes.
     """
     salt = enbase64(urandom(byte_size))
     return salt
 
 
 def check_password_hash(password, password_hash, salt):
-    """Given a password, hash, salt this function verifies the password is equal to hash/salt.
+    """
+    Given a password, hash, salt this function verifies the password is equal to hash/salt.
+
     Args:
-        password (str): The password to check.
+       - ``password``: The password to perform check on.
 
     Returns:
-        bool
+       - ``bool``
     """
-
     if PYTHON3 and isinstance(salt, bytes):
         password = str(password)
         salt = salt.decode('utf-8')
