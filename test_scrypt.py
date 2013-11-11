fd8e9c5ebeb6a6bbb4292b1cc6d22b510141c834
Nick Robinson <grobins2@gmail.com>
2013-11-11 04:23:26 -0500

    basic test

--- test_scrypt.py
+++ test_scrypt.py
@@ -3,11 +3,32 @@ import unittest
 from flask_scrypt import generate_random_salt, generate_password_hash, check_password_hash
 
 class ScryptTestCase(unittest.TestCase):
+    def setUp(self):
+        self.salt = generate_random_salt()
+        self.password = 'mypassword'
+        self.password_hash = generate_password_hash(self.password, self.salt)
 
     def test_is_string(self):
         salt = generate_random_salt()
-        print(type(str(salt))
         self.assertTrue(isinstance(salt, str))
+
+    def test_check_password_correct(self):
+        password = 'mypassword'
+        salt = self.salt
+        password_hash = generate_password_hash(password, salt)
+        self.assertTrue(check_password_hash(password, password_hash, salt))
+
+    def test_check_password_incorrect(self):
+        incorrect_password = 'notmypassword'
+        salt = self.salt
+        password_hash = self.password_hash
+        self.assertFalse(check_password_hash(incorrect_password, password_hash, salt))
+
+    def test_password_fail_with_incorrect_salt(self):
+        password = self.password
+        password_hash = self.password_hash
+        salt = generate_random_salt()
+        self.assertFalse(check_password_hash(password, password_hash, salt))
 
 if __name__ == '__main__':
     unittest.main()
